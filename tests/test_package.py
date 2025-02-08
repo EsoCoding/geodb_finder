02b5600f1c8b5992f09da337e22d1bee0f078512
esocode <esocode@horarylab.nl>
2025-02-08 01:34:16 +0100

    chore: add root directory to Python path in test file

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the root directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 import asyncio
 from geodb_finder import search_location, search_location_async
 
